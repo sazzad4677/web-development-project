86a72538fce36056a11befd3122e1f38a2dd1d0f
Md. Sazzad Hossain <36146119+sazzad4677@users.noreply.github.com>
2020-08-21 19:03:02 +0600

    Here I created HomeScreen and ProductScreen
    Here homescreen's and productscreen's data comes from data.js.
    Also, here i linked homescreen and product screen.

---
+++ frontend/frontend/src/data.js
@@ -0,0 +1,50 @@
+export default {
+    products:[
+    {
+        _id:1,
+        name: 'Half Silk',
+        category:'Silk',
+        image: '/images/d1.jpg',
+        price: 1200,
+        brand: 'Arong',
+        rating: 4.5,
+        numReviews:5
+
+    },
+    {
+        _id:2,
+        name: 'Full Silk',
+        category:'Silk',
+        image: '/images/d1.jpg',
+        price: 1400,
+        brand: 'Arong',
+        rating: 4.5,
+        numReviews:7
+
+    },
+    {
+        _id:3,
+        name: 'Jamdani',
+        category:'Jamdani',
+        image: '/images/d1.jpg',
+        price: 1300,
+        brand: 'Arong',
+        rating: 4.5,
+        numReviews:6
+
+    },
+    {
+        _id:4,
+        name: 'Jamdani',
+        category:'Jamdani',
+        image: '/images/d1.jpg',
+        price: 1300,
+        brand: 'Arong',
+        rating: 4.5,
+        numReviews:6
+
+    },
+
+
+]
+}
